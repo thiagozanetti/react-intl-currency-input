c72c8917f244e8ce783cd0b092318c22e21a6e63
Thiago <thiago.project@gmail.com>
2017-02-22 12:26:25 -0400

    Adding common events handling

--- src/index.jsx
+++ src/index.jsx
@@ -28,26 +28,50 @@ class IntlCurrencyInput extends Component {
   normalizeValue = str => {
     // strips everything that is not a number (positive or negative).
     return Number(str.replace(/[^0-9-]/g, ""))
-  }
+  };
 
   handleChange = event => {
     event.preventDefault();
 
     // value must be divided by 100 to work with cents.
-    const value = normalizeValue(event.target.value) / 100;
+    const value = this.normalizeValue(event.target.value) / 100;
     const maskedValue = formatCurrency(value, this.props.config, this.props.currency);
 
     this.setState({
       maskedValue,
     });
 
-    this.props.onChange(event, value, maskedValue);
-  }
+    if (this.props.onChange) {
+      this.props.onChange(event, value, maskedValue);
+    }
+  };
+
+  handleBlur = event => {
+    if (this.props.onBlur) {
+      this.props.onBlur(event);
+    }
+  };
+
+  handleFocus = event => {
+    if (this.props.onFocus) {
+      this.props.onFocus(event);
+    }
+  };
+
+  handleKeyPress = event => {
+    if (this.props.onKeyPress) {
+      this.props.onKeyPress(event);
+    }
+  };
 
   render() {
     return(
       <input value={this.state.maskedValue}
-        onChange={this.handleChange} />
+        onChange={this.handleChange}
+        onBlur={this.handleBlur}
+        onFocus={this.handleFocus}
+        onKeyUp={this.handleKeyPress}
+     />
     );
   }
 };
@@ -56,6 +80,9 @@ IntlCurrencyInput.propTypes = {
   currency: React.PropTypes.string.isRequired,
   config: React.PropTypes.object.isRequired,
   onChange: React.PropTypes.func,
+  onBlur: React.PropTypes.func,
+  onFocus: React.PropTypes.func,
+  onKeyPress: React.PropTypes.func,
 };
 
 IntlCurrencyInput.defaultProps = {
