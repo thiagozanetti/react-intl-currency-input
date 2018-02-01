5a377b096f4897c0fa91f5d6ea0f9dd3150b8910
Thiago Zanetti <thiago.project@gmail.com>
2018-02-01 19:05:59 -0200

    Updating example.

--- example/src/index.jsx
+++ example/src/index.jsx
@@ -2,14 +2,14 @@ import React, { Component } from "react"
 import ReactDom from "react-dom"
 
 import SyntaxHighlighter, { registerLanguage } from "react-syntax-highlighter/dist/light"
-import javascript from "highlight.js/lib/languages/javascript"
-import gist from "react-syntax-highlighter/dist/styles/github-gist"
-
-registerLanguage("javascript", javascript);
+import javascript from "react-syntax-highlighter/languages/hljs/javascript"
+import gist from "react-syntax-highlighter/dist/styles/hljs/github-gist"
 
 import IntlCurrencyInput from "../../src"
 
 import "./index.css"
+
+registerLanguage("javascript", javascript);
 
 const currencyConfig = {
   locale: "pt-BR",
@@ -28,24 +28,25 @@ const currencyConfig = {
 // a little hack to make highlighting work.
 const stripKeyQuotes = strObj => {
   const regex = /"([\w]+)":/g;
-  const subst = `\$1:`;
+  const subst = `$1:`;
 
   return strObj.replace(regex, subst);
 };
 
+const defaultValue = 1234.56;
+
 class BrlCurrencyInput extends Component {
-  defaultValue = 1234.56;
 
   constructor(props) {
     super(props);
 
     this.state = {
-      value: this.defaultValue,
+      value: defaultValue,
       maskedValue: "R$1234,56",
     };
   }
 
-  handleChange = (event, value, maskedValue) => {
+  handleChange (event, value, maskedValue) {
     event.preventDefault();
 
     console.log(value); // value without mask (ex: 1234.56)
@@ -59,13 +60,13 @@ class BrlCurrencyInput extends Component {
   };
 
   render() {
-    return(
+    return (
       <div>
         <IntlCurrencyInput currency="BRL" config={currencyConfig}
-                defaultValue={this.defaultValue}
-                onChange={this.handleChange}
-                autoFocus={true}
-                autoSelect={true}
+          defaultValue={defaultValue}
+          onChange={ (ev, value, maskedValue) => this.handleChange(ev, value, maskedValue) }
+          autoFocus={true}
+          autoSelect={true}
         />
         <p>value: <strong>{this.state.value}</strong></p>
         <p>maskedValue: <strong>{this.state.maskedValue}</strong></p>
