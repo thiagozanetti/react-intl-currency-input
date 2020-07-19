a52fdf59c31a284a21ebddd953bc10b83798c956
Thiago Zanetti <zanetti@devgrid.co.uk>
2020-07-19 00:46:36 -0300

    Using a new default value

--- example/src/index.js
+++ example/src/index.js
@@ -33,7 +33,7 @@ const stripKeyQuotes = strObj => {
   return strObj.replace(regex, subst);
 };
 
-const defaultValue = 10.00;
+const defaultValue = 2.5;
 const maxValue = 999999999.99;
 
 const BrlCurrencyInput = () => {
