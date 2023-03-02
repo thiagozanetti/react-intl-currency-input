9217cbbc5bffe2b110e5a47ff488ed97825d3859
Thiago Zanetti <thiago.project@gmail.com>
2023-03-01 23:09:50 -0300

    Adding types

--- lib/format-currency.ts
+++ lib/format-currency.ts
@@ -1,7 +1,10 @@
 import Big from "big.js";
 
+import { IntlFormatterConfig } from "./types";
+
+export default function formatCurrency(value: number, localeConfig: IntlFormatterConfig, currencyName: string) {
   const numberConfig = localeConfig.formats.number[currencyName];
-  const formatter = new global.Intl.NumberFormat(localeConfig.locale, numberConfig);
+  const formatter = new globalThis.Intl.NumberFormat(localeConfig.locale, numberConfig);
 
   return formatter.format(Big(value).toNumber());
 }
