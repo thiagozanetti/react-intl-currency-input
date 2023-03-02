3b42422c5cff515464be08f29bbecce6ea7412d1
Thiago Zanetti <thiago.project@gmail.com>
2023-03-01 23:07:18 -0300

    Adding big.js to work with precision float points

--- lib/format-currency.ts
+++ lib/format-currency.ts
@@ -1,6 +1,7 @@
-export default function formatCurrency(value: number, localeConfig, currencyName) {
+import Big from "big.js";
+
   const numberConfig = localeConfig.formats.number[currencyName];
   const formatter = new global.Intl.NumberFormat(localeConfig.locale, numberConfig);
 
-  return formatter.format(BigInt(value));
+  return formatter.format(Big(value).toNumber());
 }
