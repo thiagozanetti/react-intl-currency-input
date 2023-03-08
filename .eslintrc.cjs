27c435cf08117d8037ad54e7e2a7f43238838862
Thiago Zanetti <thiago.project@gmail.com>
2023-03-08 20:14:15 -0300

    Moving to a js object

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,42 @@
 module.exports = {
-  'env': {
+  env: {
     'browser': true,
     'es2021': true,
     'node': true
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:@typescript-eslint/recommended',
   ],
-  'overrides': [
+  overrides: [
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  'plugins': [
+  plugins: [
     'react',
     '@typescript-eslint'
   ],
-  'rules': {
-    'indent': [
+  rules: {
+    indent: [
       'error',
       2
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'always'
     ],
     'linebreak-style': [
       'error',
       'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
     ],
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': 'error',
